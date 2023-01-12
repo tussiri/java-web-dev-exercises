9cbebe591b2f2fe27cda3d02da17b1c83444cbfe
Tuma Ussiri <tussiri@gmail.com>
2023-01-12 09:20:59 +0000

    completed class 1 and 2 exercises

--- src/Studios/Class2/CharCounter.java
+++ src/Studios/Class2/CharCounter.java
@@ -9,19 +9,26 @@ import java.util.Scanner;
 public class CharCounter {
     public static void main(String[] args) {
         String startingString = "If the product of two terms is zero then common sense says at least one of the two terms has to be zero to start with. So if you move all the terms over to one side, you can put the quadratics into a form that can be factored allowing that side of the equation to equal zero. Once you’ve done that, it’s pretty straightforward from there.";
-        char[] charactersInString = startingString.toCharArray();
-        count(charactersInString);
-        System.out.println(charactersInString);
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Please enter a string: ");
+        String userString = input.nextLine();
+
+        char[] characterArray = userString.toLowerCase().toCharArray();
+        count(characterArray);
+        System.out.println(characterArray);
+
+
 
     }
 
-    public static void count(char[] charactersInString) {
+    public static void count(char[] characterArray) {
         HashMap<Character, Integer> countChar = new HashMap<>();
-        for (int i = 0; i < charactersInString.length; i++) {
+        for (int i = 0; i < characterArray.length; i++) {
             int counter = 0;
-            char test = charactersInString[i];
-            for (int j = 0; j < charactersInString.length; j++) {
-                if (charactersInString[j] == test) {
+            char test = characterArray[i];
+            for (int j = 0; j < characterArray.length; j++) {
+                if (characterArray[j] == test) {
                     counter++;
                 }
             }
